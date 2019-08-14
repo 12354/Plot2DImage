1491a2e1fb6fd6c56c59faf7a67b60f2c79b018f
Marcel Kiesel <marci66@gmail.com>
2019-08-14 14:40:54 +0200

    Plot Base Commit

--- Plot2DImage/Form1.Designer.cs
+++ Plot2DImage/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.width = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.width)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(90, 11);
+            this.width.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(120, 20);
+            this.width.TabIndex = 0;
+            this.width.Value = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Width/Height";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "load imkage";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.width);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.width)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown width;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Plot2DImage/Form1.cs
+++ Plot2DImage/Form1.cs
@@ -3,18 +3,107 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FastBitmapLib;
 
 namespace Plot2DImage
 {
+    struct DataLine
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float Intensity { get; set; }
+    }
+    struct PolarIntensity
+    {
+        public float HZ { get; set; }
+        public float Z { get; set; }
+        public float Intensity { get; set; }
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ReadFile(ofd.FileName);
+            }
+        }
+
+        private void ReadFile(string ofdFileName)
+        {
+            var content = File.ReadAllLines(ofdFileName);
+            var converted = content.Select(ConvertLine).ToList();
+            var polar = converted.Select(ConvertToPolarCoordinates).ToList();
+            Bitmap i = new Bitmap((int) width.Value,(int)width.Value);
+            using (var fastBitmap = i.FastLock())
+            {
+                /*for (int x = 0; x < width.Value; x++)
+                {
+                    for (int y = 0; y < width.Value; y++)
+                    {
+                        i.SetPixel(x,y,Color.Magenta);
+                    }
+                }*/
+                fastBitmap.Clear(Color.Magenta);
+                foreach (var polarIntensity in polar)
+                {
+                    var x = ConvertToSquarePixel(polarIntensity.HZ);
+                    var y = ConvertToSquarePixel(polarIntensity.Z);
+                    var grayValue = (int) ((polarIntensity.Intensity + 20) / 30 * 255);
+                    grayValue = Math.Min(255, grayValue);
+                    fastBitmap.SetPixel(x, y, Color.FromArgb(255, grayValue, grayValue, grayValue));
+                    //i.SetPixel(x, y, Color.FromArgb(255, grayValue, grayValue, grayValue));
+                }
+            }
+
+            i.Save("output.bmp");
+        }
+
+        private int ConvertToSquarePixel(float polarIntensity)
+        {
+            return (int)(polarIntensity / (Math.PI * 2) * (double) width.Value);
+        }
+
+        private PolarIntensity ConvertToPolarCoordinates(DataLine arg)
+        {
+            var len = Math.Sqrt(arg.X * arg.X + arg.Y * arg.Y + arg.Z * arg.Z);
+            var hz = Math.Atan2(arg.Y, arg.X);
+            var z = Math.Cos(arg.Z / len);
+            if (hz < 0)
+            {
+                hz += 2 * Math.PI;
+            }
+            return new PolarIntensity()
+            {
+                HZ = (float) hz,
+                Z = (float) z,
+                Intensity = arg.Intensity
+            };
+        }
+
+        private DataLine ConvertLine(string arg)
+        {
+            var split = arg.Split(',').Select(str => float.Parse(str,CultureInfo.InvariantCulture)).ToArray();
+            return new DataLine()
+            {
+                X = split[0],
+                Y = split[1],
+                Z = split[2],
+                Intensity = split[3],
+            };
         }
     }
 }
